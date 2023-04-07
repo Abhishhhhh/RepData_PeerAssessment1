05cdfc3efca1ac9de8b09ea953d7a2fc5f830b54
Abhishek Reddy <127029931+Abhishhhhh@users.noreply.github.com>
2023-04-07 20:25:39 +0530

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## Caching the Mean of a Vector [Divyansh Kulshreshtha]
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<-y
+    inv <<-NULL
+  }
+  get <- function()x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set=set, get = get,
+       setinverse = setinverse,
+       getinverse=getinverse
+       )
+
+}
+
+
+## Write a short comment describing this function
+
+cacheinverse <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  matrix_to_inverse <- x$get()
+  inv <- solve(matrix_to_inverse, ...)
+  x$setinverse(inv)
+  inv
+      ## Return a matrix that is the inverse of 'x'
+}
